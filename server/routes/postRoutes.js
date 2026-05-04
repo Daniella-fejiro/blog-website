b89fd83e2943f5ad204be6c93925408a36f2e01d
Daniella Fejiro <daniellaifeakachukwu@gmail.com>
2026-05-04 20:02:26 +0100

    changed filenames

--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Post = require("../models/post");
+const Post = require("../models/Post");
 
 router.post("/", async (req, res) => {
     const post = new Post(req.body);
